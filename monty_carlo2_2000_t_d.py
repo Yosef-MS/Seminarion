88e6b58837a05978007e0c1ce4c3e35c92f08765
Yosef <yosefstanton@outlook.com>
2025-06-23 18:36:20 +0300

    move values over from excel. in last 2 commits also the plant life and USD/NIS exchange were wrong

--- monty_carlo2_2000_t_d.py
+++ monty_carlo2_2000_t_d.py
@@ -17,7 +17,7 @@ print("Plant power rating (MWh/y):", plant_power_rating)
 
 # CAPEX and OPEX
 capex = np.random.normal(loc=585, scale=206, size=n_simulations) * plant_size * cap_fac_assumed*365 #USD/t*y * t/d * d/y
-opex = capex/10
+opex = np.random.normal(loc=58.5, scale=20.6, size=n_simulations) * plant_size * cap_fac_assumed*365 #vary independently
 
 # Variable assumptions
 cal_val_waste = np.random.normal(loc=11.48, scale=0.60, size=n_simulations)
@@ -25,15 +25,15 @@ elec_effic = np.random.normal(loc=0.25, scale=0.05, size=n_simulations)
 cap_fac = np.random.normal(loc=0.841, scale=0.0822, size=n_simulations)
 
 # Revenue 
-elec_tariff = 0.64 / 3.62  # USD/kWh
+elec_tariff = 0.64 / 3.5  # USD/kWh
 elec_prod_MWh_y = cal_val_waste * plant_size * elec_effic * 365 * cap_fac / 3.6
 elec_prod_kWh_y = elec_prod_MWh_y * 1000
-revenue = elec_prod_kWh_y * elec_tariff
+revenue = elec_prod_kWh_y * elec_tariff + 15083465 #extra USD/y from externalities
 print("Average annual revenue (USD):", np.mean(revenue))
 
 # NPV calculation
 discount_rate = np.random.normal(loc=0.065, scale=0.035, size=n_simulations)
-plant_life = 22
+plant_life = 23
 npv = -capex + np.sum(
     [(revenue - opex) / (1 + discount_rate)**t for t in range(1, plant_life + 1)],
     axis=0
@@ -67,7 +67,7 @@ for bin_left, bin_right, patch in zip(bins[:-1], bins[1:], patches):
 plt.axvline(x=0, color='black', linestyle='--', label='NPV = 0')
 plt.xlabel("Net Present Value (USD)")
 plt.ylabel("Frequency")
-plt.title("NPV - 2000 t/d")
+plt.title("NPV Total Benefit - 2000 t/d")
 plt.legend()
 
 # Add bold percentage text in plot
